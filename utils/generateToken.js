fe1285a0514538c1fd3e3684ed92476c6c0c15df
Kavita Soren <165135963+kavitasoren02@users.noreply.github.com>
2025-10-08 18:50:14 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId, role) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000,
     })
 
